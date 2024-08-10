3d93d15b09d3ac8f0e6ed5f2fdf4a8209dc7739a
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-08-09 23:13:28 -0300

    DevBurger#7

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import UserControler from './app/controllers/UserController';
-
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 routes.post('/users', UserControler.store);
+routes.post('/session', SessionController.store);
 
 export default routes;
