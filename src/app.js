951d3f71bf6bb4cd8769e7c2929c574cf4fe23f4
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-08-11 09:59:56 -0300

    DevBurguer#8

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { resolve } from 'node:path';
+
 import routes from './routes';
 
 import './database';
@@ -13,6 +15,10 @@ class App {
 
   middlewares() {
     this.app.use(express.json());
+    this.app.use(
+      '/product-file',
+      express.static(resolve(__dirname, '..', 'uploads')),
+    );
   }
 
   routes() {
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserControler from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProductController from './app/controllers/ProductController';
 
 const routes = new Router();
 
+const upload = multer(multerConfig);
+
 routes.post('/users', UserControler.store);
 routes.post('/session', SessionController.store);
+routes.post('/products', upload.single('file'), ProductController.store);
+routes.get('/products', ProductController.index)
 
 export default routes;
