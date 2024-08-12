aa1c749cb384201067cfae18b1e66de1935dab3c
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-08-12 19:57:41 -0300

    DevBurger#9

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,23 +1,24 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(request, response) {
     const schema = Yup.object({
-        email: Yup.string().email().required(),
-        password: Yup.string().min(6).required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
     });
-    
 
     const isValid = await schema.isValid(request.body);
 
     const emailOrPasswordIncorrect = () =>
       response
         .status(401)
-        .json({ error: 'Make sure your email or password are correct'})
+        .json({ error: 'Make sure your email or password are correct' });
 
     if (!isValid) {
-        return emailOrPasswordIncorrect();
+      return emailOrPasswordIncorrect();
     }
 
     const { email, password } = request.body;
@@ -30,20 +31,23 @@ class SessionController {
 
     if (!user) {
       return emailOrPasswordIncorrect();
-  }
+    }
 
     const isSamePassword = await user.checkPassword(password);
 
     if (!isSamePassword) {
       return emailOrPasswordIncorrect();
     }
-    
-    return response.status(201).json({ 
+
+    return response.status(201).json({
       id: user.id,
       name: user.name,
       email,
       admin: user.admin,
-     });
+      token: jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
+import authMiddleware from './middlewares/auth';
 
 import UserControler from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
@@ -12,7 +13,9 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserControler.store);
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductController.store);
-routes.get('/products', ProductController.index)
+routes.get('/products', ProductController.index);
 
 export default routes;
