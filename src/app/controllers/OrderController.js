875e205ab2bc6d750dc5044a9e636e72eb68b03e
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-08-17 21:29:42 -0300

    DevBurger#12

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -2,6 +2,7 @@ import * as Yup from 'yup';
 import Order from '../schemas/Order';
 import Product from '../models/Products';
 import Category from '../models/Category';
+import User from '../models/User';
 
 class OrderController {
   async store(request, response) {
@@ -60,9 +61,47 @@ class OrderController {
         name: request.userName,
       },
       products: formattedProducts,
+      status: 'Pedido realizado',
     };
 
-    return response.status(201).json(order);
+    const createdOrder = await Order.create(order);
+
+    return response.status(201).json(createdOrder);
+  }
+
+  async index(request, response) {
+    const orders = await Order.find();
+
+    return response.json(orders);
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      status: Yup.string().required(),
+    });
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId);
+
+    if (!isAdmin) {
+      return response.status(401).json();
+    }
+
+    const { id } = request.params;
+    const { status } = request.body;
+
+    try {
+      await Order.updateOne({ _id: id }, { status });
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+
+    return response.json({ message: 'Status updated sucessfully' });
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -19,10 +19,14 @@ routes.post('/session', SessionController.store);
 routes.use(authMiddleware);
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index);
+routes.put('/products/:id', upload.single('file'), ProductController.update);
 
 routes.post('/categories', CategoryController.store);
 routes.get('/categories', CategoryController.index);
 
 routes.post('/orders', OrderController.store);
+routes.get('/orders', OrderController.index);
+routes.put('/orders/:id', OrderController.update);
+
 
 export default routes;
