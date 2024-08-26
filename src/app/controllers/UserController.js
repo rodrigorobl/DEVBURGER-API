3dfda49b0df91a8d76dbc53c75b7fa6982f5a236
rodrigorobl <rodrigo.augusto.r21@gmail.com>
2024-08-26 18:19:43 -0300

    DevBurgerAPINote#2

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,50 +1,50 @@
-import { v4 } from 'uuid';
-import * as Yup from 'yup';
+import { v4 } from "uuid";
+import * as Yup from "yup";
 
-import User from '../models/User';
+import User from "../models/User";
 
 class UserControler {
-  async store(request, response) {
-    const schema = Yup.object({
-      name: Yup.string().required(),
-      email: Yup.string().email().required(),
-      password: Yup.string().min(6).required(),
-      admin: Yup.boolean(),
-    });
+	async store(request, response) {
+		const schema = Yup.object({
+			name: Yup.string().required(),
+			email: Yup.string().email().required(),
+			password: Yup.string().min(6).required(),
+			admin: Yup.boolean(),
+		});
 
-    try {
-      schema.validateSync(request.body, { abortEarly: false });
-    } catch (err) {
-      return response.status(400).json({ error: err.errors });
-    }
+		try {
+			schema.validateSync(request.body, { abortEarly: false });
+		} catch (err) {
+			return response.status(400).json({ error: err.errors });
+		}
 
-    const { name, email, password, admin } = request.body;
+		const { name, email, password, admin } = request.body;
 
-    const userExists = await User.findOne({
-      where: {
-        email,
-      },
-    });
+		const userExists = await User.findOne({
+			where: {
+				email,
+			},
+		});
 
-    if (userExists) {
-      return response.status(409).json({ error: 'User already exists' });
-    }
+		if (userExists) {
+			return response.status(409).json({ error: "User already exists" });
+		}
 
-    const user = await User.create({
-      id: v4(),
-      name,
-      email,
-      password,
-      admin,
-    });
+		const user = await User.create({
+			id: v4(),
+			name,
+			email,
+			password,
+			admin,
+		});
 
-    return response.status(201).json({
-      id: user.id,
-      name,
-      email,
-      admin,
-    });
-  }
+		return response.status(201).json({
+			id: user.id,
+			name,
+			email,
+			admin,
+		});
+	}
 }
 
 export default new UserControler();
