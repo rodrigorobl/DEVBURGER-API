7e7fba3e1ff5cbd01f8ef5a3ef2591d4a469d3a5
Your Name <you@example.com>
2025-05-22 16:16:42 -0300

    ajustemongodb

--- src/server.js
+++ src/server.js
@@ -1,3 +1,23 @@
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
+const mongoURI = 'mongodb+srv://rodrigoaugustor21:5xQ3k3BAD%23pKBCe@cluster0.by5ulwn.mongodb.net/devburger';
+
+async function startServer() {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('MongoDB conectado com sucesso');
+
+    app.listen(3001, () => {
+      console.log('Servidor rodando na porta 3001');
+    });
+  } catch (error) {
+    console.error('Erro na conexão com MongoDB:', error.message);
+    process.exit(1);
+  }
+}
+startServer();
+
 module.exports = app;
+
